04e17e32a8b5a3efc2884e5f4f9bd1d8fc34fb1e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-20 16:22:20 -0700

    Added unit tests

---
+++ tests.py
@@ -0,0 +1,58 @@
+"""Testsq for Balloonicorn's Flask app."""
+
+import unittest
+import party
+
+
+class PartyTests(unittest.TestCase):
+    """Tests for my party site."""
+
+    def setUp(self):
+        """Code to run before every test."""
+
+        self.client = party.app.test_client()
+        party.app.config['TESTING'] = True
+
+    def test_homepage(self):
+        """Can we reach the homepage?"""
+
+        result = self.client.get("/")
+        self.assertIn("having a party", str(result.data))
+
+    def test_no_rsvp_yet(self):
+        """Do users who haven't RSVPed see the correct view?"""
+
+        result = self.client.get("/")
+        self.assertIn("<h2>Please RSVP</h2>", str(result.data))
+        # FIXME: Add a test to show we haven't RSVP'd yet
+        print "No RSVP test works."
+
+    def test_rsvp(self):
+        """Do RSVPed users see the correct view?"""
+
+        rsvp_info = {'name': "Jane", 'email': "jane@jane.com"}
+
+        result = self.client.post("/rsvp", data=rsvp_info,
+                                  follow_redirects=True)
+        self.assertIn("<h2>Party Details</h2>", str(result.data))
+        
+        self.assertNotIn('<input type="submit" value="Oooh! I want to come!" class="btn btn-primary">', 
+                        str(result.data))
+        # FIXME: check that once we log in we see party details--but not the form!
+        
+        print "RSVP Test works"
+
+    def test_rsvp_mel(self):
+        """Can we keep Mel out?"""
+
+        # FIXME: write a test that mel can't invite himself
+        result = self.client.get("/")
+        self.assertTrue(party.is_mel("Mel Melitpolski", "some email"))
+        self.assertTrue(party.is_mel("Mel Melitpolski", "mel@ubermelon.com"))
+        self.assertTrue(party.is_mel("Melonhead", "mel@ubermelon.com"))
+        self.assertFalse(party.is_mel("Caroline", "caro@line.com"))
+        print "No Mel test works"
+
+
+if __name__ == "__main__":
+    unittest.main()
